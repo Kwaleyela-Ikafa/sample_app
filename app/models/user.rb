9788528ced213f9ab484e43b1a365a81bc4078f8
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-02-12 22:18:17 +0200

    Add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
                       uniqueness: 
                       
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
