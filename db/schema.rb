94dd28124ed8d7b9668f09ec6d12debfccde281e
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-02-12 13:52:22 +0200

    Add indexes to user email

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_064511) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_12_114017) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
