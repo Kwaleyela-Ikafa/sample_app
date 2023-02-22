724b634e76b84f1921b995d9a48594350fcf8faf
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-02-22 08:56:58 +0200

    Added redirect to user page after creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      # Handle succesful save
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
